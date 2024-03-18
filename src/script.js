a674a13eb945115eda1ff459fe52a6c7d9a541b4
James Ian Juban <jamesiansama0924@gmail.com>
2024-03-18 12:37:55 +0800

    trial

--- src/script.js
+++ src/script.js
@@ -1,4 +1,4 @@
-   // Define the student object
+// Define the student object
    let student = {
     name: "James Ian Juban",
     age: 24,
@@ -10,4 +10,34 @@
 };
 
 // Call the displayInfo method to show information about the student
-student.displayInfo();
+    student.displayInfo();
+
+    loader();
+
+    document.querySelector('.btn').onclick = loader;
+    
+    /*====================================
+    *     LOADER
+    ======================================*/
+    function loader(_success) {
+        var obj = document.querySelector('.preloader'),
+        inner = document.querySelector('.preloader_inner'),
+        page = document.querySelector('.page');
+        obj.classList.add('show');
+        page.classList.remove('show');
+        var w = 0,
+            t = setInterval(function() {
+                w = w + 1;
+                inner.textContent = w+'%';
+                if (w === 100){
+                    obj.classList.remove('show');
+                    page.classList.add('show');
+                    clearInterval(t);
+                    w = 0;
+                    if (_success){
+                        return _success();
+                    }
+                }
+            }, 20);
+    }
+    
